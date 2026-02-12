f68bfdba6d459b2d2ea028c3d18924feaf3616ca
Petr Hlista <131767638+PetrH630@users.noreply.github.com>
2026-02-12 08:22:41 +0100

    Doplněny komentáře u Exceptions

--- backend/src/main/java/cz/phsoft/hokej/exceptions/InvalidAdminActivateDeactivateException.java
+++ backend/src/main/java/cz/phsoft/hokej/exceptions/InvalidAdminActivateDeactivateException.java
@@ -2,9 +2,28 @@ package cz.phsoft.hokej.exceptions;
 
 import org.springframework.http.HttpStatus;
 
+/**
+ * Výjimka signalizující neplatný pokus administrátora
+ * o aktivaci nebo deaktivaci entity.
+ *
+ * Používá se v servisní vrstvě v situacích,
+ * kdy požadovaná operace není z hlediska
+ * aplikačních pravidel povolena.
+ *
+ * Výjimka je zpracována globálním exception handlerem
+ * a vrací HTTP status METHOD_NOT_ALLOWED.
+ *
+ * Dědí z BusinessException, která reprezentuje
+ * porušení doménových pravidel aplikace.
+ */
 public class InvalidAdminActivateDeactivateException extends BusinessException {
-    public InvalidAdminActivateDeactivateException(String message) {
 
+    /**
+     * Vytvoří výjimku s detailní chybovou zprávou.
+     *
+     * @param message text popisující důvod nepovolené operace
+     */
+    public InvalidAdminActivateDeactivateException(String message) {
         super(message, HttpStatus.METHOD_NOT_ALLOWED);
     }
 }
